249219e171913d00defb43d70cd090915ba3ae2c
Sarvesh Kumar <40709233+Sarveshgithub@users.noreply.github.com>
2020-06-26 20:56:47 +0530

    Complete Register Screen

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,75 +1,104 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function RegisterScreen(props) {
   console.log("register props", props);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [rePassword, setRePassword] = useState("");
+  const [userdetail, setUserDetail] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  console.log("userdetail::", userdetail);
   const [userId, setUserId] = useState("");
-  console.log("userInfo::::", userId);
+  const [error, setError] = useState("");
+  const [loading, setLoad] = useState(false);
+
   useEffect(() => {
     if (userId) {
-      console.log("get user Id");
+      setUserDetail({
+        name: "",
+        email: "",
+        password: "",
+      });
       props.history.push("/signin");
     }
   }, [userId]);
 
-  const submitHandler = (e) => {
-    e.preventDefault();
+  useEffect(() => {
+    if (error && error !== "") {
+      toast.error(`Error!\n${error}`, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setError("");
+    }
+  }, [error]);
 
-    // async function fetchData() {
-    //   try {
-    //     const { data } = await Axios.post("/api/users/register", {
-    //       name,
-    //       email,
-    //       password,
-    //     });
-    //   } catch (error) {
-    //     console.log("error:::", error);
-    //   }
-    // }
-    // fetchData();
-    Axios.post("/api/users/register", {
-      name,
-      email,
-      password,
-    })
+  const submitHandler = (e) => {
+    setLoad(true);
+    e.preventDefault();
+    Axios.post("/api/users/register", userdetail)
       .then((response) => {
+        setLoad(false);
         // Success
         setUserId(response.data.response._id);
         console.log(response);
+        toast.success("Success!\nRegister Sucessfully!", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
       })
       .catch((error) => {
-        console.log("datamesssage", error.message);
-        // Error
         if (error.response) {
-          console.log("1data", error.response.data);
-          console.log("1status", error.response.status);
-          console.log("1headers", error.response.headers);
+          setLoad(false);
+          setError(error.response.data.message);
         }
       });
   };
   return (
     <div className="form">
+      <div>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div>
       <form onSubmit={submitHandler}>
         <ul className="form-container">
           <li>
             <h2>Create Account</h2>
           </li>
-          {/* <li>
-            {loading && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-          </li> */}
+          <li>{loading ? <div>Loading...</div> : ""}</li>
           <li>
             <label htmlFor="name">Name</label>
             <input
               type="name"
               name="name"
               id="name"
-              onChange={(e) => setName(e.target.value)}
+              value={userdetail.name}
+              onChange={(e) =>
+                setUserDetail({ ...userdetail, name: e.target.value })
+              }
             ></input>
           </li>
           <li>
@@ -78,7 +107,10 @@ function RegisterScreen(props) {
               type="email"
               name="email"
               id="email"
-              onChange={(e) => setEmail(e.target.value)}
+              value={userdetail.email}
+              onChange={(e) =>
+                setUserDetail({ ...userdetail, email: e.target.value })
+              }
             ></input>
           </li>
           <li>
@@ -87,7 +119,10 @@ function RegisterScreen(props) {
               type="password"
               id="password"
               name="password"
-              onChange={(e) => setPassword(e.target.value)}
+              value={userdetail.password}
+              onChange={(e) =>
+                setUserDetail({ ...userdetail, password: e.target.value })
+              }
             ></input>
           </li>
           <li>
@@ -96,18 +131,28 @@ function RegisterScreen(props) {
               type="password"
               id="rePassword"
               name="rePassword"
-              onChange={(e) => setRePassword(e.target.value)}
+              // onChange={(e) => setRePassword(e.target.value)}
             ></input>
           </li>
           <li>
-            <button type="submit" className="button primary">
+            <button
+              type="submit"
+              disabled={
+                userdetail.name !== "" &&
+                userdetail.email !== "" &&
+                userdetail.password !== ""
+                  ? false
+                  : true
+              }
+              className="button primary"
+            >
               Register
             </button>
           </li>
           <li>
             Already have an account?
             <Link to={"/signin"} className="button secondary text-center">
-              Create your amazona account
+              Sign In
             </Link>
           </li>
         </ul>
